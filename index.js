2b838a7d2335d4249cd35d806a2cfecc5a6829c8
Granit_700 <114147712+Granit-700@users.noreply.github.com>
2026-08-12 22:19:11 +0600

    refactor(auth): clean up auth controller

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors({ credentials: true, origin: "http://localhost:5500" }));
+app.use(cors({ credentials: true, origin: process.env.CLIENT_URL }));
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth", authRouter);
